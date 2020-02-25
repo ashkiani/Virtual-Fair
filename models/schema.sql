b47a365ab87e9c4029b96b1e558cda06df851346
Gene Edwards <eedwards@su.edu>
2020-02-24 21:33:20 -0500

    Updated database schema file to include virtual_fair_db

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,2 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
-
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+DROP DATABASE IF EXISTS virtual_fair_db;
+CREATE DATABASE virtual_fair_db;
