22ce34c2e0ac6a453e844851f3e8b26c97cbfee4
Sia <ashkiani@yahoo.com>
2020-02-29 12:23:43 -0500

    Added the initial /api/jobs post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 var db = require("../models");
 
 module.exports = function (app) {
-  app.get('/api/jobs', async (req, res) => {
+  app.get("/api/jobs", async (req, res) => {
     try {
       console.log(db.tblJobs);
       db.tblJobs.findAll({}).then(function (allJobs) {
@@ -11,15 +11,15 @@ module.exports = function (app) {
       console.log(err);
       res.send("Error occurred:" + err);
     }
-  })
+  });
 
-  app.post('/api/jobs', async (req, res) => {
+  app.post("/api/jobs", async (req, res) => {
     try {
+      console.log("api post received at /api/jobs");
       //Postman test
-      // {
-      //   "cities": [1, 2, 3],
-      //     "skills": [4, 5, 6],
-      //       "keywords": ["k1", "k2", "k3"]
+      // {"cities": [1, 2, 3],
+      //  "skills": [4, 5, 6],
+      //  "keywords": ["k1", "k2", "k3"]
       // }
       console.log(req.body);
       console.log(req.body.cities);
@@ -31,12 +31,27 @@ module.exports = function (app) {
       //   // response.status(404, 'The task is not found').send();
       //   res.json(foundJobs);
       // });
-
+      //it returns an array of job objects that include job id, job title and job skills and job requirements.
+      //object below is for initial testing only. The plan is to populate it from the database.
+      let result = [];
+      result.push({
+        jobId: "1",
+        jobTitle: "test job1",
+        jobSkills: ["skill1", "skill2"],
+        jobRequirements: ["req1", "req2"]
+      });
+      result.push({
+        jobId: "2",
+        jobTitle: "test job2",
+        jobSkills: ["skill1", "skill3"],
+        jobRequirements: ["req5", "req2"]
+      });
+      res.json(result);
     } catch (err) {
       console.log(err);
       res.send("Error occurred:" + err);
     }
-  })
+  });
 
   // Get all examples
   // app.get("/api/examples", function (req, res) {
