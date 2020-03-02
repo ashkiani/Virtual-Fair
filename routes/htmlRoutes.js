a009073de8450e511fc536bfc4ece0ab5dac9f92
Sia <ashkiani@yahoo.com>
2020-03-02 17:00:31 -0500

    Resolving conflicts with master branch

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,24 +1,17 @@
-var db = require("../models");
+// var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+  app.get("/", (req, res) => res.render("index"));
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+  // Load Jobs page
+  app.get("/jobs", (req, res) => res.render("jobs"));
+
+  // Load Status page
+  app.get("/status", (req, res) => res.render("status")); //res.send('Status page -- UNDER CONSTRUCTION!')
+
+  // Load admin page
+  app.get("/admin", (req, res) => res.render("admin"));
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
