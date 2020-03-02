416d466cfc824336509c9175223304ec3800fafa
Siavash Ashkiani <sashkiani@iteris.com>
2020-03-02 14:23:43 -0500

    Added some sample data to test my recent change for fixing the relationships

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,18 +1,33 @@
--- Insert into jobs table
-Insert into virtual_fair_db.jobs (title, description, createdAt, updatedAt)
-VALUES('Developer', 'Developing web applications', Now(), Now());
+INSERT INTO virtual_fair_db.actions SET action='reviewing',createdAt=now(),updatedAt=now();
+INSERT INTO virtual_fair_db.actions SET action='reviewed',createdAt=now(),updatedAt=now();
+INSERT INTO virtual_fair_db.actions SET action='approved',createdAt=now(),updatedAt=now();
+INSERT INTO virtual_fair_db.actions SET action='rejected',createdAt=now(),updatedAt=now();
 
--- Insert into skills table
-Insert into virtual_fair_db.skills (skill, createdAt, updatedAt)
-Values('Software Development', Now(), Now());
+INSERT INTO virtual_fair_db.usertypes SET type='applicant',createdAt=now(),updatedAt=now();
+INSERT INTO virtual_fair_db.usertypes SET type='admin',createdAt=now(),updatedAt=now();
 
--- Insert into job skills table
-Insert into virtual_fair_db.jobskills (createdAt, updatedAt, jobid, skillid)
-values (Now(), Now(), 1, 1);
+INSERT INTO virtual_fair_db.users SET firstname='applicant_firstname',lastname='applicant_lastname',username='applicant1',password='password1',createdAt=now(),updatedAt=now();
+INSERT INTO virtual_fair_db.users SET firstname='admin_firstname',lastname='admin_lastname',username='admin1',password='password2',createdAt=now(),updatedAt=now();
 
--- Insert into actions table
-Insert into virtual_fair_db.tblactions (action, createdAt, updatedAt)
-Values ('Schedule Interview', Now(), Now()), 
-('Offer Job', Now(), Now()),
-('Interviewed', Now(), Now()), 
-('Not Qualified', Now(), Now())
+INSERT INTO virtual_fair_db.userstypes SET UserId=1,UserTypeId=1,createdAt=now(),updatedAt=now();
+INSERT INTO virtual_fair_db.userstypes SET UserId=2,UserTypeId=2,createdAt=now(),updatedAt=now();
+
+INSERT INTO virtual_fair_db.skills SET skill='JavaScript',createdAt=now(),updatedAt=now();
+INSERT INTO virtual_fair_db.skills SET skill='HTML',createdAt=now(),updatedAt=now();
+INSERT INTO virtual_fair_db.skills SET skill='CSS',createdAt=now(),updatedAt=now();
+
+INSERT INTO virtual_fair_db.requirements SET requirement='bachelor degree',createdAt=now(),updatedAt=now();
+
+INSERT INTO virtual_fair_db.locations SET location='Arlington',createdAt=now(),updatedAt=now();
+
+INSERT INTO virtual_fair_db.jobs SET title='Web Developer',description='This a fron-end position',locationId=1 ,createdAt=now(),updatedAt=now();
+
+INSERT INTO virtual_fair_db.jobskills SET jobId=1, skillId=1,createdAt=now(),updatedAt=now();
+INSERT INTO virtual_fair_db.jobskills SET jobId=1, skillId=2,createdAt=now(),updatedAt=now();
+INSERT INTO virtual_fair_db.jobskills SET jobId=1, skillId=3,createdAt=now(),updatedAt=now();
+
+INSERT INTO virtual_fair_db.jobrequirements SET jobId=1, requirementId=1,createdAt=now(),updatedAt=now();
+
+INSERT INTO virtual_fair_db.applications SET jobId=1, userId=1,note='Im very interested at this job.',createdAt=now(),updatedAt=now();
+
+INSERT INTO virtual_fair_db.adminactions SET actionId=1,applicationId=1, userId=2,note='Seems like a very good candidate.',createdAt=now(),updatedAt=now();
