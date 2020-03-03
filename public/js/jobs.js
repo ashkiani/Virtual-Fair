aa44a6f2452c2ed0c3297eb65c106af18dd146c5
Marvin Castro <bbmarvin2003@yahoo.com>
2020-03-03 15:33:19 -0500

    adding on clink event

---
+++ public/js/jobs.js
@@ -0,0 +1,18 @@
+// On-click Call for Location, Skills and Keyword search
+$("#search-button").on("click", function() {
+  // save the character they typed into the character-search input
+  var searchJobs = $("#jobSearch")
+    .val()
+    .trim();
+
+  console.log(searchJobs);
+
+  // AJAX - search by keyword
+  $.ajax("/api/jobs", {
+    type: "POST"
+  }).then(function(data) {
+    console.log(data);
+    // code to display compare searchJobs to Database data
+    // Then display on page
+  })
+});
