0fa844ec7d33a3a82a4b81bfb43752c570f50f5a
Siavash Ashkiani <sashkiani@iteris.com>
2020-03-03 15:33:28 -0500

    Added code to pass locations and skills to the jobs page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,26 @@
-// var db = require("../models");
+const db = require("../models");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", (req, res) => res.render("index"));
 
   // Load Jobs page
-  app.get("/jobs", (req, res) => res.render("jobs"));
+  app.get("/jobs", async (req, res) => {
+    //Siavash 3/3/2020 Added the following calls to pass location and skills to the jobs page.
+    db.Locations.findAll({}).then(function(dbLocations) {
+      let foundLocations = dbLocations.map(val => {
+        return { id: val.id, location: val.location };
+      });
+      db.Skills.findAll({}).then(function(dbSkills) {
+        let foundSkills = dbSkills.map(val => {
+          return { id: val.id, skill: val.skill };
+        });
+        let values = { locations: foundLocations, skills: foundSkills };
+        console.log(values);
+        res.render("jobs", values);
+      });
+    });
+  });
 
   // Load Status page
   app.get("/status", (req, res) => res.render("status")); //res.send('Status page -- UNDER CONSTRUCTION!')
