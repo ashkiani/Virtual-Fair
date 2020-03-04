9923ecd6d9297b3bb6bd81e55a728074d3e5c0ba
Marvin Castro <bbmarvin2003@yahoo.com>
2020-03-04 18:20:11 -0500

    adding ajax to jobs page

--- public/js/jobs.js
+++ public/js/jobs.js
@@ -1,4 +1,4 @@
-// On-click Call for Location, Skills and Keyword search
+// On-click AJAX call for Keyword search
 $("#search-button").on("click", function() {
   // save the character they typed into the character-search input
   var searchJobs = $("#jobSearch")
@@ -9,10 +9,71 @@ $("#search-button").on("click", function() {
 
   // AJAX - search by keyword
   $.ajax("/api/jobs", {
-    type: "POST"
+    type: "GET"
   }).then(function(data) {
     console.log(data);
-    // code to display compare searchJobs to Database data
-    // Then display on page
+    //render job results
+    // loadJobs(data);
   });
 });
+
+// // On-click AJAX call for Locations search
+// $("#search-button").on("click", function() {
+//   // save the character they typed into the character-search input
+//   var searchJobs = $("#jobSearch")
+//     .val()
+//     .trim();
+
+//   console.log(searchJobs);
+
+//   // AJAX - search by keyword
+//   $.ajax("/api/jobs", {
+//     type: "GET"
+//   }).then(function(data) {
+//     console.log(data);
+//     // code to display compare searchJobs to Database data
+//     // Then display on page
+//   });
+// });
+
+// // On-click AJAX call for Skills search
+// $("#search-button").on("click", function() {
+//   // save the character they typed into the character-search input
+//   var searchJobs = $("#jobSearch")
+//     .val()
+//     .trim();
+
+//   console.log(searchJobs);
+
+//   // AJAX - search by keyword
+//   $.ajax("/api/jobs", {
+//     type: "GET"
+//   }).then(function(data) {
+//     console.log(data);
+//     // code to display compare searchJobs to Database data
+//     // Then display on page
+//   });
+// });
+
+var mainDiv = $("#mainDiv");
+    mainDiv.html("");
+
+function loadJobs() {
+  if (data.length > 0) {
+    for (var i = 0; i < data.length; i++) {
+      var divOne = $("<div>").attr("class", "card job-card");
+      mainDiv.append(divOne);
+      var jobTitle = $("<h5>").attr("class", "card-header").css({"border-bottom", "1px solid #fff"}).text(data[i].jobTitle);
+      divOne.append(jobTitle);
+      var divTwo = $("<div>").attr("class","card-body");
+      mainDiv.append(divTwo);
+      var location = $("<h5>").("class","card-title").text(data[1].location);
+      divTwo.append(location);
+      var hr = $("<hr>").css("background-colo","#DC4749");
+      divTwo.append("hr");
+
+      
+
+    }
+  }
+}
