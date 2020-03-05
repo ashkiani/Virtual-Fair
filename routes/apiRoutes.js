e2dd82002e3bfa2ac6fdf8707f74006f4d598aad
Siavash Ashkiani <sashkiani@iteris.com>
2020-03-05 09:14:41 -0500

    Refactored the job search code for api/jobs (both GET and POST)

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,54 +1,58 @@
 const db = require("../models");
+function getJobs(condition, res) {
+  //Siavash 3/4/2020: I saw a couple of issue with using .findAll({ include: { all: true, nested: true }})
+  //1- It returns more than what is needed and it has poor performance
+  //2- It wasn't working for Skills - it complained about a missing relationship but we know the relationships exists and works correctly
+  // Hence I added a couple of nested queries to retrieve only what is required for this route.
+  db.Jobs.findAll({
+    include: [db.JobSkills, db.JobRequirements],
+    where: condition
+  }).then(async allJobs => {
+    let output = [];
+    for (let i = 0; i < allJobs.length; i++) {
+      let location = await db.Locations.findOne({
+        attributes: ["location"],
+        where: { id: allJobs[i].LocationId }
+      });
+      let skillIds = allJobs[i].JobSkills.map(jobSkill => {
+        return jobSkill.SkillId;
+      });
+      let skills = await db.Skills.findAll({
+        attributes: ["skill"],
+        where: { id: skillIds }
+      });
+      skills = skills.map(skill => {
+        return skill.skill;
+      });
+      let ReqIds = allJobs[i].JobRequirements.map(JobRequirement => {
+        return JobRequirement.RequirementId;
+      });
+      let requirements = await db.Requirements.findAll({
+        attributes: ["requirement"],
+        where: { id: ReqIds }
+      });
+      requirements = requirements.map(requirement => {
+        return requirement.requirement;
+      });
+      output.push({
+        id: allJobs[i].id,
+        title: allJobs[i].title,
+        description: allJobs[i].description,
+        location: location.location,
+        skills: skills,
+        requirements: requirements
+      });
+    }
+    console.log(output);
+    res.status(200).json(output);
+  });
+}
 
-module.exports = function (app) {
+module.exports = function(app) {
   app.get("/api/jobs", async (req, res) => {
     try {
       console.log("api get received at /api/jobs");
-      //Siavash 3/4/2020: I saw a couple of issue with using .findAll({ include: { all: true, nested: true }})
-      //1- It returns more than what is needed and it has poor performance
-      //2- It wasn't working for Skills - it complained about a missing relationship but we know the relationships exists and works correctly
-      // Hence I added a couple of nested queries to retrieve only what is required for this route.
-      db.Jobs.findAll({ include: [db.JobSkills, db.JobRequirements] }).then(
-        async allJobs => {
-          let output = [];
-          for (let i = 0; i < allJobs.length; i++) {
-            let location = await db.Locations.findOne({
-              attributes: ["location"],
-              where: { id: allJobs[i].LocationId }
-            });
-            let skillIds = allJobs[i].JobSkills.map(jobSkill => {
-              return jobSkill.SkillId;
-            });
-            let skills = await db.Skills.findAll({
-              attributes: ["skill"],
-              where: { id: skillIds }
-            });
-            skills = skills.map(skill => {
-              return skill.skill;
-            });
-            let ReqIds = allJobs[i].JobRequirements.map(JobRequirement => {
-              return JobRequirement.RequirementId;
-            });
-            let requirements = await db.Requirements.findAll({
-              attributes: ["requirement"],
-              where: { id: ReqIds }
-            });
-            requirements = requirements.map(requirement => {
-              return requirement.requirement;
-            });
-            output.push({
-              id: allJobs[i].id,
-              title: allJobs[i].title,
-              description: allJobs[i].description,
-              location: location.location,
-              skills: skills,
-              requirements: requirements
-            });
-          }
-          console.log(output);
-          res.status(200).json(output);
-        }
-      );
+      getJobs(null, res);
     } catch (err) {
       console.log(err);
       res.send("Error occurred:" + err);
@@ -62,60 +66,39 @@ module.exports = function (app) {
       // {
       //   "locations": [1,2,3],
       //   "skills":[1,5,6],
-      //   "keywords":["web","JavaScript","cool"]
+      //   "keywords":"web"
       //   }
       console.log(req.body);
-      console.log(req.body.cities);
+      console.log(req.body.locations);
       console.log(req.body.skills);
       console.log(req.body.keywords);
+      let keywords = `%${req.body.keywords}%`;
+      // if (req.body.keywords !== null) {
+      //   if (req.body.keywords.length > 0) {
+      //     keywordsArr = [];
+      //     req.body.keywords.forEach(element => {
+      //       keywordsArr.push(`{ title: { [Op.like]: "%${element}%" } }`);
+      //       keywordsArr.push(`{ description: { [Op.like]: "%${element}%" } }`);
+      //     });
+      //     keywords = keywordsArr.join(); //`{[Op.or]: ['${keywordsArr.join()}']}`;
+      //     console.log(keywords);
+      //   }
+      // }
       const Op = db.Sequelize.Op;
-      db.Jobs.findAll({
-        include: [db.JobSkills, db.JobRequirements],
-        where: {
-          [Op.or]: [{ title: { [Op.like]: "%sdfeb%" } }, { title: { [Op.like]: "%rweb%" } }]
+      const condition = {
+        [Op.and]: [
+          { "$JobSkills.SkillId$": req.body.skills },
+          { LocationId: req.body.locations },
+          {
+            [Op.or]: [
+              { title: { [Op.like]: keywords } },
+              { description: { [Op.like]: keywords } }
+            ]
+          }
+        ]
+      };
 
-        }
-      }).then(async allJobs => {
-        let output = [];
-        console.log(allJobs.length);
-        for (let i = 0; i < allJobs.length; i++) {
-          let location = await db.Locations.findOne({
-            attributes: ["location"],
-            where: { id: allJobs[i].LocationId }
-          });
-          let skillIds = allJobs[i].JobSkills.map(jobSkill => {
-            return jobSkill.SkillId;
-          });
-          let skills = await db.Skills.findAll({
-            attributes: ["skill"],
-            where: { id: skillIds }
-          });
-          skills = skills.map(skill => {
-            return skill.skill;
-          });
-          let ReqIds = allJobs[i].JobRequirements.map(JobRequirement => {
-            return JobRequirement.RequirementId;
-          });
-          let requirements = await db.Requirements.findAll({
-            attributes: ["requirement"],
-            where: { id: ReqIds }
-          });
-          requirements = requirements.map(requirement => {
-            return requirement.requirement;
-          });
-          output.push({
-            id: allJobs[i].id,
-            title: allJobs[i].title,
-            description: allJobs[i].description,
-            location: location.location,
-            skills: skills,
-            requirements: requirements
-          });
-        }
-        console.log(output);
-        res.status(200).json(output);
-      }
-      );
+      getJobs(condition, res);
     } catch (err) {
       console.log(err);
       res.send("Error occurred:" + err);
