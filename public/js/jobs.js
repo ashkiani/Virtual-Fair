d390a9ff6b95ce08ca503eacf49900b45ae62d80
Marvin Castro <bbmarvin2003@yahoo.com>
2020-03-06 14:31:55 -0500

    changes

--- public/js/jobs.js
+++ public/js/jobs.js
@@ -1,3 +1,12 @@
+$.ajax("/api/jobs", {
+  type: "GET"
+}).then(function(data) {
+  console.log(data);
+  //render job results
+  loadJobs(data);
+});
+
+
 // On-click AJAX call for Keyword search
 $("#search-button").on("click", function() {
   // save the character they typed into the character-search input
@@ -6,16 +15,8 @@ $("#search-button").on("click", function() {
     .trim();
 
   console.log(searchJobs);
-
-  // AJAX - search by keyword
-  $.ajax("/api/jobs", {
-    type: "GET"
-  }).then(function(data) {
-    console.log(data.length);
-    //render job results
-    loadJobs(data);
-  });
 });
+// AJAX - search by keyword
 
 // // On-click AJAX call for Locations search
 // $("#search-button").on("click", function() {
@@ -58,45 +59,43 @@ $("#search-button").on("click", function() {
 function loadJobs(data) {
   var mainDiv = $("#mainDiv").html("");
 
-  if (data.length > 0) {
-    for (var i = 0; i < data.length; i++) {
-      var divOne = $("<div>").attr("class", "card job-card");
-      mainDiv.append(divOne);
-      var jobTitle = $("<h5>")
-        .attr("class", "card-header")
-        .css({ "border-bottom": "1px solid #fff" })
-        .text(data[i].Title);
-      divOne.append(jobTitle);
-      var divTwo = $("<div>").attr("class", "card-body");
-      divOne.append(divTwo);
-      var location = $("<h5>")
-        .attr("class", "card-title")
-        .text(data[i].Location);
-      divTwo.append(location);
-      var hr = $("<hr>").css("background-colo", "#DC4749");
-      divTwo.append(hr);
-      var p = $("<p>")
-        .attr("class", "card-text")
-        .text(data[i].Description);
-      divTwo.append(p);
-      var apply = $("<button>")
-        .attr("type", "button")
-        .attr("id", "applyJob")
-        .attr("class", "btn btn-secondary")
-        .attr("data-toggle", "tooltip")
-        .attr("title", "Apply");
-      var i = $("<i>").attr("class", "far fa-check-square fa-2x");
-      apply.append(i);
-      divTwo.append(apply);
-      var save = $("<button>")
-        .attr("type", "button")
-        .attr("id", "applyJob")
-        .attr("class", "btn btn-secondary")
-        .attr("data-toggle", "tooltip")
-        .attr("title", "Save");
-      var i2 = $("<i>").attr("class", "far fa-star fa-2x");
-      save.append(i2);
-      divTwo.append(save);
-    }
+  for (var i = 0; i < data.length; i++) {
+    var divOne = $("<div>").attr("class", "card job-card");
+    mainDiv.append(divOne);
+    var jobTitle = $("<h5>")
+      .attr("class", "card-header")
+      .css({ "border-bottom": "1px solid #fff" })
+      .text(data[i].Title);
+    divOne.append(jobTitle);
+    var divTwo = $("<div>").attr("class", "card-body");
+    divOne.append(divTwo);
+    var location = $("<h5>")
+      .attr("class", "card-title")
+      .text(data[i].Location);
+    divTwo.append(location);
+    var hr = $("<hr>").css("background-colo", "#DC4749");
+    divTwo.append(hr);
+    var p = $("<p>")
+      .attr("class", "card-text")
+      .text(data[i].Description);
+    divTwo.append(p);
+    var apply = $("<button>")
+      .attr("type", "button")
+      .attr("id", "applyJob")
+      .attr("class", "btn btn-secondary")
+      .attr("data-toggle", "tooltip")
+      .attr("title", "Apply");
+    var i = $("<i>").attr("class", "far fa-check-square fa-2x");
+    apply.append(i);
+    divTwo.append(apply);
+    var save = $("<button>")
+      .attr("type", "button")
+      .attr("id", "applyJob")
+      .attr("class", "btn btn-secondary")
+      .attr("data-toggle", "tooltip")
+      .attr("title", "Save");
+    var i2 = $("<i>").attr("class", "far fa-star fa-2x");
+    save.append(i2);
+    divTwo.append(save);
   }
 }
